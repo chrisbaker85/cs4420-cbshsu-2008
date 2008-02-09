ca8403c2b17dcad8abfc95e5cc15d7bb768bad7e
chrisbaker85 <chrisbaker85@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-02-09 20:11:31 +0000

    adding basic classes

---
+++ trunk/src/BufferManager/BufferManager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package BufferManager;
+
+/**
+ * @author chrisb
+ *
+ */
+public class BufferManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
