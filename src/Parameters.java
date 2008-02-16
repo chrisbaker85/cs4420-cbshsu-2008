d8ed8d7c9dd2556b3e436815bd5e470bdbb4c268
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-02-16 20:32:54 +0000

    updated NUM_BLOCKS_BUFFER (200>4096)

--- src/Parameters.java
+++ src/Parameters.java
@@ -8,18 +8,23 @@
  */
 public class Parameters {
 
+	// 1KB block size
 	public static int BLOCK_SIZE=1024;
-	public static int NUM_BLOCK_BUFFER=200;
-	public static int BTREE_ORDER = 5;//example
+	
+	// The buffer pool is 16MB with 1KB blocks
+	public static int NUM_BLOCK_BUFFER=4096;
+	
+	// The order of the B+Tree
+	public static int BTREE_ORDER = 5;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println(BLOCK_SIZE);
-		System.out.println(NUM_BLOCK_BUFFER);
-		System.out.println(BTREE_ORDER);
+		System.out.println("Block Size: " + BLOCK_SIZE);
+		System.out.println("# of Blocks in the Buffer: " + NUM_BLOCK_BUFFER);
+		System.out.println("B+Tree Order:" + BTREE_ORDER);
 
 	}
 
