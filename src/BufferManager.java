c20e8d98a298d3451bd61cfe5e346eae0158489b
ubaissis <ubaissis@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-02-16 22:49:41 +0000

    write

--- src/BufferManager.java
+++ src/BufferManager.java
@@ -23,27 +23,14 @@
  * - nextSlot  : indicates which block in the buffer is ready to be replaced by a new block.
  */
 
-import java.util.*;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintWriter;
-import java.io.OutputStream;
-import java.io.ObjectOutputStream;
-import java.util.Date;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.StringTokenizer;
-import java.util.Vector;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Hashtable;
 
 public class BufferManager {
 	int num_buffer = 10;
@@ -195,8 +182,31 @@ public class BufferManager {
 	 * block in the memory is the same as that in the disc. If the status is not
 	 * updated, it does nothing.
 	 */
-	private void writeBlock(long blockID) {
+	private void writeBlock(long blockID)
+	{
+		Block temp = null;
 		
+		if (lookupTable.contains(blockID)) {
+			
+			temp = buffer[((Integer) lookupTable.get(blockID)).intValue()];
+			
+			if (temp != null)
+			{
+				try {
+					FileInputStream fin = new FileInputStream();
+					FileChannel fc = fin.getChannel();
+					MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 0, 1024);
+					temp.isUpdated = false;
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
 	}
 
 	/**
