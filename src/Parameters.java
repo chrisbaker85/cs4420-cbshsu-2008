aee6ba1f2e25271f25da8a73e90b51f079f7e07c
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-02-16 23:02:08 +0000

    added tupleheadersize and blockheadersize

--- src/Parameters.java
+++ src/Parameters.java
@@ -8,14 +8,30 @@
  */
 public class Parameters {
 
-	// 1KB block size
-	public static int BLOCK_SIZE=1024;
+	/**
+	 * 1KB block size
+	 */
+	public static int BLOCK_SIZE = 1024;
 	
-	// The buffer pool is 16MB with 1KB blocks
-	public static int NUM_BLOCK_BUFFER=4096;
+	/**
+	 * The buffer pool is 16MB with 1KB blocks
+	 */
+	public static int NUM_BLOCK_BUFFER = 4096;
 	
-	// The order of the B+Tree
+	/**
+	 * The order of the B+Tree
+	 */
 	public static int BTREE_ORDER = 5;
+	
+	/**
+	 * The size of the tuple header in bytes
+	 */
+	public static int TUPLE_HEADER_SIZE = 1;
+	
+	/**
+	 * The size of the block header in bytes 
+	 */
+	public static int BLOCK_HEADER_SIZE = 1;
 	
 	/**
 	 * @param args
@@ -24,7 +40,9 @@ public class Parameters {
 		
 		System.out.println("Block Size: " + BLOCK_SIZE);
 		System.out.println("# of Blocks in the Buffer: " + NUM_BLOCK_BUFFER);
-		System.out.println("B+Tree Order:" + BTREE_ORDER);
+		System.out.println("B+Tree Order: " + BTREE_ORDER);
+		System.out.println("Tuple Header Size: " + TUPLE_HEADER_SIZE);
+		System.out.println("Block Header Size: " + BLOCK_HEADER_SIZE);
 
 	}
 
