4361dfea482fc0f57079728d07213e7d10daa2b5
sovandyhang <sovandyhang@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-02-24 08:33:05 +0000

    Finished the implementation.

--- src/SystemCatalog.java
+++ src/SystemCatalog.java
@@ -1,14 +1,40 @@
 import java.util.Hashtable;
 
-
 public class SystemCatalog {
 
 	/**
 	 * @param args
 	 */
 	
-	private Hashtable attributeCatalog;
+	// private Hashtable attributeCatalog;
+	private String db_name;
 	private Hashtable relationCatalog;
+	
+	public SystemCatalog(String db_name)
+	{
+		this.db_name = db_name;
+		relationCatalog = new Hashtable();
+	}
+	/*
+	public Hashtable getAttributeCatalog()
+	{
+		return this.attributeCatalog;
+	}
+	public void addAttributeCatalog(String key, Attribute attObj)
+	{
+		this.attributeCatalog.put(key, attObj);
+	}
+	*/
+	
+	public Hashtable getRelationCatalog()
+	{
+		return this.relationCatalog;
+	}
+	
+	public void addRelationCatalog(String key, RelationInfo relObj)
+	{
+		this.relationCatalog.put(key, relObj);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
