80b095e78020bfbee7a93ef3e73e453c50ea8b6c
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-02-29 19:31:07 +0000

    duplicates var added to insert index query

--- src/QueryEngine.java
+++ src/QueryEngine.java
@@ -12,7 +12,7 @@ public interface QueryEngine {
 	
 	public void insertQuery(String table_name, String [][] query);
 	public void createTable(String db_name, String table_name, String [][] attributes);
-	public void createIndexQuery(String index_name, String table_name, String field_name);
+	public void createIndexQuery(String index_name, String table_name, String field_name, boolean duplicates);
 	public void selectQuery(String table_name, String [] fields, String[][] where);
 	public void selectIndexQuery(String table_name, String index_name);
 	public void selectCatalogQuery();
