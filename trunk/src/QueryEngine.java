b36065ffc01b0335b53acc72d48437bf01f911d7
chrisbaker85 <chrisbaker85@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-02-29 19:30:13 +0000

    updated select query

--- trunk/src/QueryEngine.java
+++ trunk/src/QueryEngine.java
@@ -13,7 +13,7 @@ public interface QueryEngine {
 	public void insertQuery(String table_name, String [][] query);
 	public void createTable(String db_name, String table_name, String [][] attributes);
 	public void createIndexQuery(String index_name, String table_name, String field_name);
-	public void selectQuery(String table_name, String [] fields, String from, String where);
+	public void selectQuery(String table_name, String [] fields, String[][] where);
 	public void selectIndexQuery(String table_name, String index_name);
 	public void selectCatalogQuery();
 	
