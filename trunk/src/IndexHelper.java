44bba1042c03b9a4e6e8155e43ead02481499f00
ubaissis <ubaissis@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-03-07 17:57:15 +0000

    IndexHelper as a Hashtable

--- trunk/src/IndexHelper.java
+++ trunk/src/IndexHelper.java
@@ -1,317 +1,100 @@
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
+import java.io.FileOutputStream;
+import java.util.Hashtable;
 
 public class IndexHelper {
-	
-	public static int numberOfBlocks = 1 + (Parameters.BTREE_ORDER + 1)	+ (Parameters.BTREE_ORDER + 1) * (Parameters.BTREE_ORDER + 1);
-	public static int sizeOfBlockHeader = 8 * 4;
-	public static int sizeOfBlockKeys = 8 * Parameters.BTREE_ORDER;
-	public static int sizeOfBlockPointers = 8 * (Parameters.BTREE_ORDER + 1);
-	public static int sizeOfBlock = sizeOfBlockHeader +  sizeOfBlockKeys + sizeOfBlockPointers;
-	public static int sizeOfIndexFile = numberOfBlocks * sizeOfBlock;
-	public static int blockCapacity = 1 + Parameters.BTREE_ORDER;
-	
-	
-	public int rootBlockNum;
-	public int firstLeafBlockNum;
-
-	protected MappedByteBuffer buffer;
-	protected RandomAccessFile file;
+	Hashtable<Integer, Long> bTree = null;
+	String name;
 
 	public IndexHelper() {
-		rootBlockNum = -1;
-		firstLeafBlockNum = -1;
+		name = null;
+		bTree = null;
 	}
 
-	public IndexHelper(String name) {
-		rootBlockNum = -1;
-		firstLeafBlockNum = -1;
+	public IndexHelper(String aName) {
+		name = aName;
+		bTree = new Hashtable<Integer, Long>();
 		try {
-			file = new RandomAccessFile(name + ".index", "rw");
-			FileChannel fileChannel = file.getChannel();
-			buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0,
-					sizeOfBuffer);
-			buffer.putInt (rootBlockNum);
-			buffer.putInt (firstLeafBlockNum);
-			buffer.load();
+			FileOutputStream os = new FileOutputStream(name + ".Index");
+			XMLEncoder encoder = new XMLEncoder(os);
+			encoder.writeObject(bTree);
+			encoder.close();
 		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	public static IndexHelper readIndexFile(String name) {
+	public Hashtable<Integer, Long> getBTree() {
+		return bTree;
+	}
+
+	public void setBTree(Hashtable<Integer, Long> tree) {
+		bTree = tree;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static IndexHelper read(String aName) {
 		IndexHelper retVal = new IndexHelper();
+		retVal.setName(aName);
 		try {
-			retVal.file = new RandomAccessFile(name + ".index", "rw");
-			FileChannel fileChannel = retVal.file.getChannel();
-			retVal.buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0,
-					sizeOfBuffer);
-			retVal.buffer.load();
+			FileInputStream os = new FileInputStream(aName + ".Index");
+			XMLDecoder decoder = new XMLDecoder(os);
+			Hashtable<Integer, Long> tree = (Hashtable<Integer, Long>) decoder
+					.readObject();
+			retVal.setBTree(tree);
+			decoder.close();
 		} catch (FileNotFoundException e) {
-		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return retVal;
 	}
-	
-	public int getRootNum ()
-	{
-		return buffer.getInt (0);
+
+	public void insert(int key, long pointer) {
+		bTree.put(new Integer(key), new Long(pointer));
 	}
-	
-	public int getLeafNum ()
-	{
-		return buffer.getInt (16);
+
+	public long get(int key) {
+		return ((Long) bTree.get(new Integer(key))).longValue();
+	}
+
+	public void remove(int key) {
+		bTree.remove(new Integer(key));
+	}
+
+	public boolean contains(int key) {
+		return bTree.contains(new Integer(key));
 	}
 
 	public void update() {
-		buffer.force();
-	}
-
-	public long getBlock(int blockNum) {
-		return (long) (2 * 8 + (blockNum - 1) * sizeOfBlock);
-	}
-
-	public long[] getPointers(int blockNum, long keyValue) {
-		long[] retVal = null;
-
-		int keyOrder = getKeyOrder(blockNum, keyValue);
-
-		if (keyOrder != -1) {
-			retVal = new long[2];
-			retVal[0] = getLeftPointer(blockNum, keyOrder);
-			retVal[1] = getLeftPointer(blockNum, keyOrder);
-		}
-		return retVal;
-	}
-
-	public void setKeysCount(int blockNum, int newCount) {
-		buffer.putInt((blockNum - 1) * sizeOfBlock, newCount);
-	}
-
-	public void setPointersCount(int blockNum, int newCount) {
-		buffer.putInt((blockNum - 1) * sizeOfBlock + 8, newCount);
-	}
-
-	public void setKey(int blockNum, int keyOrder, long newValue) {
-		buffer.putLong((blockNum - 1) * sizeOfBlock + 16 + (keyOrder - 1) * 8,
-				newValue);
-	}
-
-	public void placeKey(int blockNum, long newValue) {
-		int place = 1;
-
-		while (place <= Parameters.BTREE_ORDER) {
-			if (0 == getKey(blockNum, place)) {
-				setKey(blockNum, place, newValue);
-				break;
-			}
-			place++;
-		}
-	}
-
-	public void setPointer(int blockNum, int pointerOrder, long newValue) {
-		buffer
-				.putLong((blockNum - 1) * sizeOfBlock + 16
-						+ Parameters.BTREE_ORDER * 8 + (pointerOrder - 1) * 8,
-						newValue);
-	}
-
-	public int keysCount(int blockNum) {
-		return buffer.getInt((blockNum - 1) * sizeOfBlock);
-	}
-
-	public int pointersCount(int blockNum) {
-		return buffer.getInt((blockNum - 1) * sizeOfBlock + 8);
-	}
-
-	public long getKey(int blockNum, int keyOrder) {
-		return buffer.getLong((blockNum - 1) * sizeOfBlock + 16
-				+ (keyOrder - 1) * 8);
-	}
-
-	public long getKey(long pointer) {
-		return buffer.getLong((int) pointer);
-	}
-
-	public boolean hasKey(int blockNum, long keyValue) {
-		int i = 1;
-		boolean retVal = false;
-
-		while (i <= Parameters.BTREE_ORDER) {
-			if (keyValue == getKey(blockNum, i)) {
-				retVal = true;
-				break;
-			}
-			i++;
-		}
-
-		return retVal;
-	}
-
-	public int getKeyOrder(int blockNum, long keyValue) {
-		int i = 1;
-		int retVal = -1;
-
-		while (i <= Parameters.BTREE_ORDER) {
-			if (keyValue == getKey(blockNum, i)) {
-				retVal = i;
-				break;
-			}
-			i++;
-		}
-
-		return retVal;
-	}
-
-	public long getPointer(int blockNum, int pointerOrder) {
-		return buffer.getLong((blockNum - 1) * sizeOfBlock + 16
-				+ Parameters.BTREE_ORDER * 8 + (pointerOrder - 1) * 8);
-	}
-
-	public long getLeftPointer(int blockNum, int keyOrder) {
-		return (long) ((blockNum - 1) * sizeOfBlock + 16
-				+ Parameters.BTREE_ORDER * 8 + (keyOrder - 1) * 8);
-	}
-
-	public long getReightPointer(int blockNum, int keyOrder) {
-		return (long) ((blockNum - 1) * sizeOfBlock + 16
-				+ Parameters.BTREE_ORDER * 8 + (keyOrder - 1) * 8 + 8);
-	}
-
-	public long getLeftPointer(int blockNum, long keyValue) {
-		int keyOrder = getKeyOrder(blockNum, keyValue);
-		return (long) ((blockNum - 1) * sizeOfBlock + 16
-				+ Parameters.BTREE_ORDER * 8 + (keyOrder - 1) * 8);
-	}
-
-	public long getReightPointer(int blockNum, long keyValue) {
-		int keyOrder = getKeyOrder(blockNum, keyValue);
-		return (long) ((blockNum - 1) * sizeOfBlock + 16
-				+ Parameters.BTREE_ORDER * 8 + (keyOrder - 1) * 8 + 8);
-	}
-
-	public long pointToKey(int blockNum, int keyOrder) {
-		return (long) ((blockNum - 1) * sizeOfBlock + 16 + (keyOrder - 1) * 8);
-	}
-
-	public boolean isFull(int blockNum) {
-		int keys = this.keysCount(blockNum);
-		int ptrs = this.pointersCount(blockNum);
-
-		if (blockNum == rootBlockNum) {
-			return (ptrs == blockCapacity);
-		}
-
-		else if (blockNum >= firstInterBlockNum && blockNum < firstLeafBlockNum) {
-			return (ptrs == blockCapacity);
-		}
-
-		else {
-			return (ptrs == blockCapacity);
-		}
-	}
-
-	public boolean isEmpty(int blockNum) {
-		int ptrs = this.pointersCount(blockNum);
-		return (ptrs == 0);
-	}
-
-	public boolean hasLimit(int blockNum) {
-		int keys = this.keysCount(blockNum);
-		int ptrs = this.pointersCount(blockNum);
-
-		if (blockNum == rootBlockNum) {
-			return (ptrs >= 2);
-		}
-
-		else if (blockNum >= firstInterBlockNum && blockNum < firstLeafBlockNum) {
-			return (ptrs >= Math.ceil(blockCapacity / 2));
-		}
-
-		else {
-			return (ptrs >= Math.floor(blockCapacity / 2));
-		}
-	}
-
-	public void printPointer(long pointerValue) {
-
-		int blockNum = (int) (pointerValue / sizeOfBlock) + 1;
-
-		System.out.print("(" + blockNum + ", " + this.getKey(pointerValue)
-				+ ")");
-
-	}
-
-	public void printBlock(int blockNum) {
-		System.out.println("Block Munmber: " + blockNum);
-
-		if (blockNum == rootBlockNum) {
-			System.out.println("Block Type: Root");
-		}
-
-		else if (blockNum >= firstInterBlockNum && blockNum < firstLeafBlockNum) {
-			System.out.println("Block Type: Inter");
-		}
-
-		else {
-			System.out.println("Block Type: Leaf");
-		}
-
-		System.out.println("Keys Count " + keysCount(blockNum));
-		System.out.println("Pointers Count " + pointersCount(blockNum));
-
-		for (int i = 1; i <= Parameters.BTREE_ORDER; i++) {
-			System.out.print("Key " + i + ": " + this.getKey(blockNum, i)
-					+ "\t");
-		}
-
-		System.out.println();
-
-		for (int i = 1; i <= Parameters.BTREE_ORDER + 1; i++) {
-			System.out.print("Pointer " + i + ": ");
-
-			if (blockNum < firstLeafBlockNum) {
-				printPointer(getPointer(blockNum, i));
-			} else {
-				if (i == Parameters.BTREE_ORDER + 1) {
-					printPointer(getPointer(blockNum, i));
-				}
-
-				else {
-					System.out.print("(" + getPointer(blockNum, i) + ")");
-				}
-			}
-
-			System.out.print("\t");
-		}
-		System.out.println();
-		System.out.println();
-
-	}
-
-	public void printIndexFile(boolean includeEmpty) {
-
-		for (int i = 1; i <= numberOfBlocks; i++) {
-
-			if (includeEmpty) {
-				printBlock(i);
-			}
-
-			else if (i == 1) {
-				printBlock(i);
-			} else if (!isEmpty(i)) {
-				printBlock(i);
-			}
-
+		try {
+			FileOutputStream os = new FileOutputStream(name + ".Index");
+			XMLEncoder encoder = new XMLEncoder(os);
+			encoder.writeObject(bTree);
+			encoder.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
-		IndexHelper test = new IndexHelper("Test");
-		test.printIndexFile(true);
+		IndexHelper helper = new IndexHelper("test");
+		helper.insert(12, 15);
+		helper.update();
+		
+		IndexHelper test = IndexHelper.read("test");
+		System.out.println(test.get(12));
 	}
-
 }
