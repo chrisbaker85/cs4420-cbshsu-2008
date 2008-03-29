68ec6f192f2f03b2b5ef49d705ede82f7db349e1
sovandyhang <sovandyhang@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-03-29 23:26:21 +0000

    Created Project class for Project operation

---
+++ trunk/src/Project.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sovandy
+ *
+ */
+public class Project implements OperationIterator {
+
+	/**
+	 * @param args
+	 */
+	
+	/*
+	public OperationIterator open(OperationIterator R)
+	{
+		return 
+	}
+	*/
+	
+	public Tuple next()
+	{
+		Tuple tuple = new Tuple();
+		return tuple;
+	}
+	public boolean hasNext() 
+	{
+		return false;
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
