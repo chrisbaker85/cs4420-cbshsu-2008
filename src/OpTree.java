1b6f17055bb6bf5a11ffeac83cd987d3beaa4279
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-04-04 21:27:23 +0000

    finished checking relations attribute-verification complete for select/project

--- src/OpTree.java
+++ src/OpTree.java
@@ -47,7 +47,7 @@ public class OpTree {
 	 * @param fields - the names of the fields (SELECT) (required)
 	 * @param where - the comparisons (WHERE) (optional)
 	 */
-	OpTree(SystemCatalog sc, String[] table_names, String[] fields, String[] where) {
+	OpTree(SystemCatalog sc, String[] table_names, String[] fields, String[][] where) {
 
 		// Set the tree's state
 		this.state = 0;
@@ -58,12 +58,12 @@ public class OpTree {
 		// Verify that all relations used in the query are valid
 		if (this.validateRelationNames(table_names)) this.valid = true;
 		
-		// Set the root, a project op
+		// Set the root, a project operation
 		this.tree_root = this.addOp(new OpProject(fields));
 		
 		if (where != null && where.length > 0) {
 			
-			// Set the second level, a select op
+			// Set the second level, a select operation
 			this.tree_root.children[0] = this.addOp(new OpSelect(where));
 			
 			// Set the third level
@@ -77,7 +77,10 @@ public class OpTree {
 			
 		}
 		
+		
 		if (this.validateAttributes(table_names)) this.valid = true;
+		
+		System.out.println(this.toString());
 		
 	}
 	
@@ -112,65 +115,65 @@ public class OpTree {
 		// individually and update it if needed
 		for (int i = 0; i < this.opList.size(); i++) {
 			
-			if (/*this.opList.get(i) instanceof OpSelect ||*/ this.opList.get(i) instanceof OpProject) {
+			Op op = this.opList.get(i);
+			
+			if (op instanceof OpSelect) {
 				
-				String att = (String)(this.opList.get(i).contents);
+				// If the op is a select, handle the attributes for
+				// each condition
+					
+				// contents is an 2-D array of relation names
+				String[][] att = ((String[][])op.contents);
 				
-				if (att.contains(".")) {
+				// Loop through the comparisons
+				for (int j = 0; j < att.length; j++) {
+				
+					// Call method to verify existence of the attribute
+					String table_name = verifyAttribute(att[j][0], table_names);
 					
-					// Check to make sure the table specified contains the attribute					
-					String table_name = att.substring(0, att.indexOf("."));
+					if (table_name == null) {
+						error = true;
+					} else {
+						att[j][0] = (table_name + "." + att[j][0]);
+					}
 					
-					if (!((RelationInfo)this.sc.getRelationCatalog().get(table_name)).hasAttribute(att.substring(att.indexOf("."), att.length()))) error = true; 
+				}
 					
-				} else {
+			} else if (op instanceof OpProject) {
+				
+				// contents is a String array of attribute names
+				String[] att = (String[])(op.contents);
+
+				// contents is null if the user indicated * in the SQL query
+				// need to project ALL fully qualified attributes of ALL the tables
+				if (att == null) {
 					
-					// No table name is specified for this attribute,
-					// so we need to resolve it (add [relation_name.] to attribute)
+					enumerateFields(table_names, i);
 					
-					// Scan through each table to see if it contains this attribute
-					for (int j = 0; j < table_names.length; j++) {
+				} else{
+					
+					// TODO: Handle verification here for each attribute
+					for (int j = 0; j < att.length; j++) {
 						
-						int instances = 0;
-						String instanceTable = null;
+						// Call method to verify existence of the attribute
+						String table_name = verifyAttribute(att[j], table_names);
 						
-						// Check if it exists in the jth table in the syscat
-						if (((RelationInfo)this.sc.getRelationCatalog().get(table_names[j])).hasAttribute(att)) {
-							
-							instances++;
-							instanceTable = table_names[j];
-							
-						}
-						
-						// Make sure that it exists exactly once
-						if (instances == 0) {
-							
+						if (table_name == null) {
 							error = true;
-							
-							// Attribute DNE
-							System.out.println("ERROR: " + att + " is not a valid attribute");
-							continue;
-							
-						} else if (instances > 1) {
-							
-							error = true;
-							
-							// Attribute specification is ambiguous
-							System.out.println("ERROR: " + att + " is ambiguous. Table must be specified.");
-							continue;
-							
+						} else {
+							att[j] = table_name + "." + att[j]; 
 						}
-						
-						this.opList.get(i).contents = instanceTable + "." + att;
 						
 					}
 					
 				}
+
 				
+			} else if (op instanceof OpIndexBasedJoin || op instanceof OpJoin || op instanceof OpSortBasedJoin) {
 				
-			} else if (this.opList.get(i) instanceof OpIndexBasedJoin || this.opList.get(i) instanceof OpJoin || this.opList.get(i) instanceof OpSortBasedJoin) {
-				
-				// TODO: NEED MORE HERE!!!!! 
+				// TODO: NEED MORE HERE!!!!!
+				// TODO: FINISH THE METHOD!!
+				// TODO: AHHHHHHHHHHHHHHH!!!
 
 				
 			}
@@ -178,6 +181,136 @@ public class OpTree {
 		}
 		
 		return !error;
+		
+	}
+	
+	/**
+	 * Check to make sure the attribute specified exists. If it does, get the
+	 * table name (even though it may already be fully qualified)
+	 * @param attr The attribute to check
+	 * @param table_names The relations to check in
+	 * @return null or relation name (String)
+	 */
+	private String verifyAttribute(String attr, String[] table_names) {
+		
+		boolean error = false;
+		String table_name = null;
+		
+		if (attr.contains(".")) {
+			
+			// Get the table name from the F.Q. attribute					
+			table_name = getTableNameFromFQAttribute(attr);
+			
+			// Check to make sure the table specified contains the attribute
+			if (!((RelationInfo)this.sc.getRelationCatalog().get(table_name)).hasAttribute(attr.substring(attr.indexOf("."), attr.length()))) error = true; 
+			
+		} else {
+			
+			// Attempt to resolve the relation name
+			table_name = this.fullyQualifyAttributeName(attr, table_names);
+			
+			if (table_name == null) {
+				error = true;
+			} else {
+				
+			}
+			
+		}
+		
+		if (error) return null;
+		return table_name;
+		
+	}
+	
+	public static String getTableNameFromFQAttribute(String attr) {
+	
+		return attr.substring(0, attr.indexOf("."));
+		
+	}
+	
+	
+	/**
+	 * Fully qualify the attribute given.  If there is an error (attribute DNE, or
+	 * is ambiguous, return null.  If o.k., return table name
+	 * @param attr the attribute we are resolving
+	 * @param table_names the tables we're checking in
+	 * @return null or table name (String)
+	 */
+	private String fullyQualifyAttributeName(String attr, String[] table_names) {
+		
+		boolean error = false;
+		String instanceTable = null;
+		int instances = 0;
+		// No table name is specified for this attribute,
+		// so we need to resolve it (add [relation_name.] to attribute)
+		
+		// Scan through each table to see if it contains this attribute
+		for (int j = 0; j < table_names.length; j++) {
+			
+			// Check if it exists in the jth table in the system catalog
+			if (((RelationInfo)this.sc.getRelationCatalog().get(table_names[j])).hasAttribute(attr)) {
+				
+				instances++;
+				instanceTable = table_names[j];
+				
+			}
+
+		}
+		
+		// Make sure that it exists exactly once
+		if (instances == 0) {
+			
+			error = true;
+			
+			// Attribute DNE
+			System.out.println("ERROR: " + attr + " is not a valid attribute");
+			
+		} else if (instances > 1) {
+			
+			error = true;
+			
+			// Attribute specification is ambiguous
+			System.out.println("ERROR: " + attr + " is ambiguous. Table must be specified.");
+			
+		}
+		
+		if (error) return null;
+
+		// else
+		return instanceTable;
+		
+	}
+	
+	/**
+	 * This function collects all the attributes from every relation for
+	 * the situation where the user has specified select *
+	 * @param table_names the names of the tables in the query
+	 * @param opIndex the index of the operator we're working on
+	 */
+	private void enumerateFields(String[] table_names, int opIndex){
+		
+		ArrayList<String> atts = new ArrayList<String>();
+		
+		// Loop through all the tables and get attribute names
+		for (int j = 0; j < table_names.length; j++) {
+			
+			Object[] ats = ((RelationInfo)this.sc.getRelationCatalog().get(table_names[j])).getAttributesAsArray();
+			
+			for (int k = 0; k < ats.length; k++) {
+				
+				atts.add(table_names[j] + "." + (String)ats[k]);
+				
+			}
+		}
+		
+		// Fill in the operator contents with the attribute objects 
+		this.opList.get(opIndex).contents = new String[atts.size()];
+		
+		for (int j = 0; j < atts.size(); j++) {
+			
+			((String[])this.opList.get(opIndex).contents)[j] = atts.get(j);
+			
+		}
 		
 	}
 	
@@ -230,7 +363,7 @@ public class OpTree {
 		String[] table_names = {"students", "courses"};
 		String[] fields = {"name, title"};
 		//String[] where = null;
-		String[] where = {"A=B", "C=D"};
+		String[][] where = {{"A", "=", "B"}, {"C", ">", "D"}};
 		
 		OpTree ot = new OpTree(null, table_names, fields, where);
 		
