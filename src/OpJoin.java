fb86b45e56c6fad2f814726f92e6f18a497d4545
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-04-16 17:44:57 +0000

    added containsRelation code

--- src/OpJoin.java
+++ src/OpJoin.java
@@ -20,6 +20,41 @@ public class OpJoin extends Op {
 	}
 	
 	/**
+	 * Helps assembly of joins from cross product
+	 * @param relationName the relation to look for in the join
+	 * @return true if this join contains the relation passed in
+	 */
+	public boolean containsRelation(String relationName) {
+		
+		// Check the children if they are OpJoins
+		// to see if one of them contains the relation
+		for (int i = 0; i < this.children.length; i++) {
+
+			if (this.children[i] instanceof OpJoin
+					&& ((OpJoin)this.children[i]).containsRelation(relationName)) {
+				return true;
+			}
+			
+		}
+
+		
+		// Check the left side of the condition for relation name
+		if (((String[])this.contents)[0].contains(relationName)) {
+			return true;
+		}
+
+		// Check the right side of the condition for relation name
+		if (((String[])this.contents)[1].contains(relationName)) {
+			return true;
+		}
+		
+		// If the relation name cannot be found, quit
+		
+		return false;
+		
+	}
+	
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
