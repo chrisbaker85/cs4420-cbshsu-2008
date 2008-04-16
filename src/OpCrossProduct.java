17ee0b9bbb8ddd897709a43794636da34a5d12ff
chrisbaker85 <chrisbaker85@users.noreply.github.com>
2008-04-16 17:42:46 +0000

    more complete code for adding joins

--- src/OpCrossProduct.java
+++ src/OpCrossProduct.java
@@ -26,35 +26,92 @@ public class OpCrossProduct extends Op {
 	}
 	
 	/**
+	 * Function that adds a join operator to the xprod op.
+	 * 
+	 * @param cond a String[] containing the join table condition
+	 */
+	public void addJoin(String[] cond) {
+		
+		String[] rem = new String[2];
+		Op[] tables;
+		
+		rem[0] = Utility.getTable(cond[0]);
+		rem[1] = Utility.getTable(cond[1]);
+		
+		tables = this.removeChildren(rem);
+		
+		if (this.children.length > 1) {
+			
+			// Find an existing OpJoin to add the new OpJoin to
+			for (int i = 0; i < this.children.length; i++) {
+				
+				// Determine if this particular child is the right one to use
+				if (this.children[i] instanceof OpJoin && (((OpJoin)this.children[i]).containsRelation(rem[0]) || ((OpJoin)this.children[i]).containsRelation(rem[1]))) {
+					
+					
+					
+					
+					
+				}
+				
+			}
+			
+		} else {
+			
+			// If there is only one child, then remove the xprod
+			
+		}
+		
+
+		
+		
+	}
+	
+	/**
 	 * Relations (OpTable) should be removed from an OpCrossProduct if they're
 	 * being added to a join op.
 	 * @param rem the Op array of relations (OpTable) to remove
 	 * @return the amended list of relations (OpTable)
 	 */
-	public void removeChildren(Op[] rem) {
+	public Op[] removeChildren(String[] rem) {
 		
-		Op[] red = new Op[this.children.length - rem.length];
-		Op[] ext = new Op[rem.length];
-		   
+		Op[] reduced = new Op[this.children.length - rem.length];
+		int reduced_idx = 0;
+		Op[] extract = new Op[rem.length];
+		int extract_idx = 0;
+		
+		// go through each child and put into proper array
 		for (int i = 0; i < this.children.length; i++) {
 			
-//			if (this.children[i]) {
-//				
-//				
-//			}
+			for (int j = 0; j < rem.length; j++) {
+				
+				if (((OpTable)(this.children[i])).contents.equals(rem[j])) {
+					
+					reduced[reduced_idx] = this.children[i];
+					reduced_idx++;
+					
+				} else {
+					
+					extract[extract_idx] = this.children[i];
+					extract_idx++;
+				}
+				
+			}
 			
 		}
 		
-		this.children = red;
+		this.children = reduced;
+		return extract;
 		
 	}
+
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
