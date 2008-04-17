853aea1ffb86a5dfdbe39a230d409562eb70531a
chrisbaker85 <chrisbaker85@ed9a468d-9645-0410-8790-e798f3a47bd9>
2008-04-17 06:59:31 +0000

    fixing join code and remove code

--- trunk/src/OpCrossProduct.java
+++ trunk/src/OpCrossProduct.java
@@ -32,37 +32,49 @@ public class OpCrossProduct extends Op {
 	 */
 	public void addJoin(String[] cond) {
 		
+
+		// STEP ONE: FIND THE TABLES AND REMOVE THEM FROM THE XPROD
+		
 		String[] rem = new String[2];
 		Op[] tables;
+		OpJoin newJoin = null;
 		
 		rem[0] = Utility.getTable(cond[0]);
 		rem[1] = Utility.getTable(cond[1]);
 		
+		System.out.println("INFO: tables to remove from xprod: " + rem[0] + "/" + rem[1]);
+		// Remove the operations that have been marked
 		tables = this.removeChildren(rem);
+		System.out.println("INFO: Removed " + tables.length + " table(s)");
 		
-		if (this.children.length > 1) {
+		
+		
+		// STEP TWO: CREATE THE JOIN OPERATOR
+		
+		if (tables.length == 1) {
 			
-			// Find an existing OpJoin to add the new OpJoin to
-			for (int i = 0; i < this.children.length; i++) {
-				
-				// Determine if this particular child is the right one to use
-				if (this.children[i] instanceof OpJoin && (((OpJoin)this.children[i]).containsRelation(rem[0]) || ((OpJoin)this.children[i]).containsRelation(rem[1]))) {
-					
-					
-					
-					
-					
-				}
-				
-			}
+			// IF ONLY ONE TABLE IS REMOVED, CREATE A JOIN TO CHAIN
+			newJoin = new OpJoin((OpJoin)this.children[0], rem[0], cond, this);
+
+			// TODO: CHAIN THE JOIN
 			
-		} else {
 			
-			// If there is only one child, then remove the xprod
+		} else if (tables.length == 2) {
+			
+			// ELSE, CREATE A NEW JOIN
+			newJoin = new OpJoin(rem[0], rem[1], cond, this);
+			// JUST ADD THE JOIN TO THE XPROD, CAN'T CHAIN
+			this.addChild(newJoin);
 			
 		}
 		
-
+		// STEP FOUR: REMOVE THE XPROD IF THERE IS ONE OP AND IT IS A JOIN
+		if (this.children.length == 1 && (this.children[0] instanceof OpJoin)) {
+			
+			this.parent.swapChildren(this, this.children[0]);
+			
+		}
+		
 		
 		
 	}
@@ -80,27 +92,48 @@ public class OpCrossProduct extends Op {
 		Op[] extract = new Op[rem.length];
 		int extract_idx = 0;
 		
-		// go through each child and put into proper array
+		// go through each child and extract those given in parameter
 		for (int i = 0; i < this.children.length; i++) {
 			
 			for (int j = 0; j < rem.length; j++) {
 				
 				if (((OpTable)(this.children[i])).contents.equals(rem[j])) {
 					
-					reduced[reduced_idx] = this.children[i];
-					reduced_idx++;
+					System.out.println("INFO: " + ((OpTable)(this.children[i])).contents + " == " + rem[j]);
+					System.out.println("INFO: extracted size: " + extract.length + "/index:" + extract_idx);
 					
-				} else {
-					
+					// Add to the "extracted" array
 					extract[extract_idx] = this.children[i];
+
+					// Make the child null so that we know it's taken already 
+					this.children[i] = null;
 					extract_idx++;
+					break;
+
 				}
 				
 			}
 			
 		}
 		
+		// go through each child and pick out non-null children
+		for (int i = 0; i < this.children.length; i++) {
+			
+			if (this.children[i] != null) {
+				
+				System.out.println("INFO: reduced size: " + reduced.length + "/index:" + reduced_idx);
+				
+				reduced[reduced_idx] = this.children[i];
+				reduced_idx++;
+				
+			}
+			
+		}
+		
+		// Make this op's child list the reduced list
 		this.children = reduced;
+		
+		// Return the extracted operations
 		return extract;
 		
 	}
